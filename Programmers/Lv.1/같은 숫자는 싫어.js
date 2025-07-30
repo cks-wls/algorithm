d307f8cdfecd03cc5d9d8f194f72d2b1755e6ca0
cks-wls <slo0521@naver.com>
2025-07-30 13:26:30 +0900

    [Lv.1] 같은 숫자는 싫어

---
+++ Programmers/Lv.1/같은 숫자는 싫어.js
@@ -0,0 +1,17 @@
+function solution(arr){
+    const result = [];
+    let hasValue = false;
+    for (let i = 0; i < arr.length; i++){
+        if (i > 0){
+            if (arr[i] === arr[i-1]){
+                hasValue = true;
+            }else{
+                hasValue = false;
+            }
+        }
+        if (!hasValue){
+            result.push(arr[i]);
+            }
+        }
+   return result; 
+}
