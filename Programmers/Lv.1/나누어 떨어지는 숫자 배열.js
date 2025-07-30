aa0492c3123662bd7a2cea3704ae85c5ab1d68f9
cks-wls <slo0521@naver.com>
2025-07-30 10:19:22 +0900

    [Lv.1] 나누어 떨어지는 숫자 배열

---
+++ Programmers/Lv.1/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,12 @@
+function solution(arr, divisor) {
+    const result = [];
+    for (let value of arr){
+        if (value % divisor === 0){
+            result.push(value);
+        }
+    }
+    if (result.length === 0){
+        result.push(-1);
+    }
+    return result.sort((a,b) => a-b);
+}
