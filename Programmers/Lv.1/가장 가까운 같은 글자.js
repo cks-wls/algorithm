0405d37c5d30f1283c6dac64ecab511abe616559
cks-wls <slo0521@naver.com>
2025-07-31 17:59:39 +0900

    [Lv.1] 가장 가까운 같은 글자

---
+++ Programmers/Lv.1/가장 가까운 같은 글자.js
@@ -0,0 +1,17 @@
+function solution(s) {
+    const result = [];
+    let value = -1;
+    for (let i = 0; i < s.length; i++){
+        for (let j = i-1; j >= 0; j--){
+            if (i !== j){
+                if (s[i] === s[j]){
+                    value = i-j;
+                    break;
+                    }
+                }   
+        }
+        result.push(value);
+        value = -1;    
+    }
+    return result;
+    }
