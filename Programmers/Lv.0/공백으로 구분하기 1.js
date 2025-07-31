9e6be04a9b09bb2f4f29be4e5c54f64f2a2d93bc
cks-wls <slo0521@naver.com>
2025-07-31 14:47:26 +0900

    [Lv.0] 공백으로 구분하기 1

---
+++ Programmers/Lv.0/공백으로 구분하기 1.js
@@ -0,0 +1,18 @@
+function solution(my_string) {
+    const result = [];
+    let str = '';
+    for (let i = 0; i < my_string.length; i++){
+        if (i !== my_string.length - 1 ){
+            if (my_string[i] !== ' '){
+                str += my_string[i];
+            }else{
+                result.push(str);
+                str = '';
+            }
+        }else{
+            str += my_string[i];
+            result.push(str);
+        }
+    }
+    return result;
+}
