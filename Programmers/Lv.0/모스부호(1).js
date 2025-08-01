09b297cba9228c1c3b95233225b476904060281b
cks-wls <slo0521@naver.com>
2025-08-02 02:18:35 +0900

    [Lv.0] 모스부호(1)

---
+++ Programmers/Lv.0/모스부호(1).js
@@ -0,0 +1,15 @@
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    };
+    let result = '';
+    const letterArr = letter.split(' ');
+    for (let value of letterArr){
+        result += morse[value]
+    }
+    return result;
+}
