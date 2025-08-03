2ba920f41cf29d817fd7b86c1a7612f373780b51
cks-wls <slo0521@naver.com>
2025-08-03 16:26:26 +0900

    [Lv.1] 숫자 문자열과 영단어

---
+++ Programmers/Lv.1/숫자 문자열과 영단어.js
@@ -0,0 +1,29 @@
+function solution(s) {
+    const number = {
+        'zero' : 0,
+        'one' : 1,
+        'two' : 2,
+        'three' : 3,
+        'four' : 4,
+        'five' : 5,
+        'six' : 6,
+        'seven' : 7,
+        'eight' : 8,
+        'nine' : 9
+    }
+    const numStr = Object.keys(number);
+    let result = '';
+    let midResult = '';
+    for (let value of s){
+        if (!isNaN(Number(value))){
+            result += value;
+        }else{
+            midResult += value;
+            if (numStr.includes(midResult)){
+                result += number[midResult];
+                midResult = '';
+            }
+     }
+    }
+    return Number(result);
+}
