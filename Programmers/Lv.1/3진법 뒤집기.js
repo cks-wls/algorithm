551cb5613c2779f7e723de2be2696c56bc2f6852
cks-wls <slo0521@naver.com>
2025-08-04 14:54:02 +0900

    [Lv.1] 3진법 뒤집기

---
+++ Programmers/Lv.1/3진법 뒤집기.js
@@ -0,0 +1,32 @@
+function solution(n) {
+    let max = 0;
+    let count = 0;
+    let result = 0;
+    let reverse = '';
+    while(3 ** max <= n){
+        max ++;
+    }
+    if (n !== 0){
+        for (let i = max-1 ; i >= 0; i--){
+        if (3**i * 2 <= n){
+            count = 2;
+            n -= 3**i * 2;
+            reverse += count;
+        }else if(3**i <= n){
+            count = 1;
+            n -= 3**i;
+            reverse += count;
+        }else{
+            count = 0;
+            reverse += count;
+        }               
+    }    
+    }else{
+        return 0;
+    }
+    
+    for (let i = 0; i < reverse.length; i++){
+        result += 3 ** i * Number(reverse[i]);
+    }
+    return result;
+}
