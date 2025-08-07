caa8bda60bfda18496e982b1521caefdff1ce144
cks-wls <slo0521@naver.com>
2025-08-07 13:29:35 +0900

    [Lv.0] 가장 큰 수 찾기

--- Programmers/Lv.0/가장 큰 수 찾기.js
+++ Programmers/Lv.0/가장 큰 수 찾기.js
@@ -1,3 +1,4 @@
+// solution 1
 function solution(array) {
     const result = [];
     let max = 0;
@@ -11,4 +12,11 @@ function solution(array) {
     result.push(max);
     result.push(maxIndex);
     return result;
+}
+// solution 2
+function solution(array) {
+    const arr = array.slice();
+    arr.sort((a,b) => b-a);
+    let max = arr[0];
+    return [arr[0], array.indexOf(arr[0])];
 }
