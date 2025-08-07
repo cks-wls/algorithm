685d10402c994b42fc5e7eb99ceb2f51d943e2f9
cks-wls <slo0521@naver.com>
2025-08-07 13:21:56 +0900

    [Lv.0] 개미 군단

--- Programmers/Lv.0/개미 군단.js
+++ Programmers/Lv.0/개미 군단.js
@@ -1,4 +1,4 @@
-// 장군개미 : 5, 병정개미 : 3, 일개미 : 1  
+// solution 1
 function solution(hp) {
     let generalAnt = 0;
     let soldierAnt = 0;
@@ -20,4 +20,23 @@ function solution(hp) {
         }
         }
     return (generalAnt + soldierAnt + workerAnt);
+}
+// solution 2
+function solution(hp) {
+    let general = 0;
+    let soldier = 0;
+    let worker = 0;
+    while (hp > 0){
+        if (hp >= 5){
+            general ++;
+            hp -= 5;
+        }else if(hp >= 3){
+            soldier ++;
+            hp -= 3;
+        }else{
+            worker ++;
+            hp -= 1;
+        }
+    }
+    return general + soldier + worker;
 }
