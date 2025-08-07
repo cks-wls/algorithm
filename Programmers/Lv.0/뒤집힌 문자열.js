3a5fee2deed26cef40dc29da008d46b7a41e9610
cks-wls <slo0521@naver.com>
2025-08-07 12:58:36 +0900

    [Lv.0] 뒤집힌 문자열

--- Programmers/Lv.0/뒤집힌 문자열.js
+++ Programmers/Lv.0/뒤집힌 문자열.js
@@ -1,7 +1,12 @@
+// solution 1
 function solution(my_string) {
     let result = ""
     for (let i = my_string.length - 1 ; i >= 0 ; i --){
         result += my_string[i];
     }
     return result;
+}
+//solution 2
+function solution(my_string) {
+    return my_string.split('').reverse().join('');
 }
