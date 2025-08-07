bcf5f10e1b9d5e20d45740058e70cc0c406ad6a9
cks-wls <slo0521@naver.com>
2025-08-07 12:56:50 +0900

    [Lv.0] 배열 뒤집기

--- Programmers/Lv.0/배열 뒤집기.js
+++ Programmers/Lv.0/배열 뒤집기.js
@@ -1,7 +1,12 @@
+// solution 1
 function solution(num_list) {
     const result = [];
     for (let value of num_list){
         result.unshift(value);
     }
     return result;
+}
+//solution 2
+function solution(num_list) {
+    return num_list.reverse();
 }
