a8b761e713d4138e18aca005443d6674cee98224
cks-wls <slo0521@naver.com>
2025-08-07 13:25:11 +0900

    [Lv.0] 문자열 정렬하기 (1)

--- Programmers/Lv.0/문자열 정렬하기(1).js
+++ Programmers/Lv.0/문자열 정렬하기(1).js
@@ -1,8 +1,20 @@
+// solution 1
 function solution(my_string) {
     let result = [];
     for (let value of my_string){
         if (Number(value) === 0 || Number(value)){
             result.push(Number(value));
+        }
+    }
+    return result.sort((a,b) => a-b);
+}
+// solution 2
+function solution(my_string) {
+    const result = [];
+    for (let value of my_string){
+        let num = Number(value);
+        if (!isNaN(num)){
+            result.push(num);
         }
     }
     return result.sort((a,b) => a-b);
