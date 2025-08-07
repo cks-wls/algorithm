b78ec717ec68fe511451cfd8ca062f128affb707
cks-wls <slo0521@naver.com>
2025-08-07 13:07:57 +0900

    [Lv.0] 배열 자르기

--- Programmers/Lv.0/배열 자르기.js
+++ Programmers/Lv.0/배열 자르기.js
@@ -1,7 +1,13 @@
+// solution 1
 function solution(numbers, num1, num2) {
     const result = [];
     for (let i = num1; i <= num2 ; i++){
         result.push(numbers[i]);
     }
+    return result;
+}
+// solution 2
+function solution(numbers, num1, num2) {
+    const result = numbers.slice(num1, num2+1);
     return result;
 }
