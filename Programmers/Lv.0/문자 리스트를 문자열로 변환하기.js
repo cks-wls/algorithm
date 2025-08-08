e5daa726c01ea636dba7a2ec31522a5c1227b360
cks-wls <slo0521@naver.com>
2025-08-08 10:20:37 +0900

    [Lv.0] 문자 리스트를 문자열로 변환하기

--- Programmers/Lv.0/문자 리스트를 문자열로 변환하기.js
+++ Programmers/Lv.0/문자 리스트를 문자열로 변환하기.js
@@ -1,7 +1,12 @@
+// solution 1
 function solution(arr) {
     let result = '';
     for (let value of arr){
         result += value;
     }
     return result;
+}
+// solution 2
+function solution(arr) {
+    return arr.join('');
 }
