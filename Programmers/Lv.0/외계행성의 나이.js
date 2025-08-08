b0cc4f4d398fea92892e5b222d1689f3023f0d85
cks-wls <slo0521@naver.com>
2025-08-08 10:25:14 +0900

    [Lv.0] 외계행성의 나이

--- Programmers/Lv.0/외계행성의 나이.js
+++ Programmers/Lv.0/외계행성의 나이.js
@@ -1,3 +1,4 @@
+// solution 1
 function solution(age) {
     let result = '';
     let planetAge = age + '';
@@ -34,6 +35,27 @@ function solution(age) {
                 result += 'j';
                 break;
         }
+    }
+    return result;
+}
+// solution 2
+function solution(age) {
+    const object = {
+        '0' : 'a', 
+        '1' : 'b', 
+        '2' : 'c', 
+        '3' : 'd', 
+        '4' : 'e', 
+        '5' : 'f', 
+        '6' : 'g', 
+        '7' : 'h', 
+        '8' : 'i', 
+        '9' : 'j', 
+    };
+    let result = '';
+    age = age + '';
+    for (let value of age){
+        result += object[value];
     }
     return result;
 }
