0c907d78f5e14710a62392c54bcaf0d886c06911
cks-wls <slo0521@naver.com>
2025-08-10 00:34:34 +0900

    [Lv.1] 2016년

---
+++ Programmers/Lv.1/2016년.js
@@ -0,0 +1,31 @@
+function solution(a, b) {
+    const date = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let totalDate = 0;
+    for (let i = 0; i < a - 1 ; i++){
+        totalDate += date[i];
+    }
+    totalDate += b;
+    switch(totalDate % 7){
+        case 0:
+            return "THU";
+            break;
+        case 1:
+            return "FRI";
+            break;
+        case 2:
+            return "SAT";           
+            break;
+        case 3:
+            return "SUN";
+            break;
+        case 4:
+            return "MON";
+            break;
+        case 5:
+            return "TUE";
+            break;
+        case 6:
+            return "WED";
+            break;
+    }
+}
