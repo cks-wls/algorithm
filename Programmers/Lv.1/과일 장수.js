f1d7bbb697ff743bf47e758d03a1a9d456eb6ff0
cks-wls <slo0521@naver.com>
2025-08-11 10:51:46 +0900

    [Lv.1] 과일 장수

---
+++ Programmers/Lv.1/과일 장수.js
@@ -0,0 +1,19 @@
+function solution(k, m, score) {
+    let result = 0;
+    score.sort((a,b) =>b-a);
+    const box = [];
+    for (let i = 0; i < score.length; i += m){
+        let middle = [];
+        for(let j = 0; j < m; j++){
+            middle.push(score[i + j]);
+        }
+         box.push(middle); 
+    }
+    for (let value of box){
+        if (!value.includes(undefined)){
+            let min = Math.min(...value);
+            result += min * m;
+        }
+    }
+    return result;
+}
