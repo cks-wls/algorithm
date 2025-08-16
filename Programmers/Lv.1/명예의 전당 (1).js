35528db6ca3dc8e31ad8c17c095daa0740b7f60f
cks-wls <slo0521@naver.com>
2025-08-17 00:09:41 +0900

    [Lv.1] 명예의 전당 (1)

---
+++ Programmers/Lv.1/명예의 전당 (1).js
@@ -0,0 +1,18 @@
+function solution(k, score) {
+  const arr = [];
+  const answer = [];
+  for (let i = 0; i < score.length; i++) {
+    if (i < k) {
+      arr.push(score[i]);
+      arr.sort((a, b) => a - b);
+      answer.push(arr[0]);
+    } else {
+      if (score[i] >= arr[0]) {
+        arr.splice(0, 1, score[i]);
+        arr.sort((a, b) => a - b);
+      }
+      answer.push(arr[0]);
+    }
+  }
+  return answer;
+}
