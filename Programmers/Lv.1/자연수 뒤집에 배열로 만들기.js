c72c53c06dd45a4fd319a0bddafcf4a29b488dcc
cks-wls <slo0521@naver.com>
2025-08-18 19:58:30 +0900

    [Lv.1] 자연수 뒤집어 배열로 만들기

--- Programmers/Lv.1/자연수 뒤집에 배열로 만들기.js
+++ Programmers/Lv.1/자연수 뒤집에 배열로 만들기.js
@@ -1,9 +1,19 @@
+// solution 1
 function solution(n) {
-    const result = [];
-    let str = '';
-    n += str;
-    for (let value of n){
-        result.unshift(Number(value));
-    }
-    return result;
+  const result = [];
+  let str = "";
+  n += str;
+  for (let value of n) {
+    result.unshift(Number(value));
+  }
+  return result;
+}
+// solution 2
+function solution(n) {
+  const arr = String(n).split("");
+  const result = [];
+  for (let value of arr) {
+    result.push(Number(value));
+  }
+  return result.reverse();
 }
