b879fa55d3fde712065f67fec2e81020ba84d323
cks-wls <slo0521@naver.com>
2025-08-28 18:53:36 +0900

    [Lv.0] 자연수 뒤집어 배열로 만들기

--- Programmers/Lv.0/가위 바위 보.js
+++ Programmers/Lv.0/가위 바위 보.js
@@ -1,14 +1,13 @@
-// 가위 : 2 바위 : 0 보 : 5
 function solution(rsp) {
-    let result = ''
-    for (let value of rsp){
-        if (value === '2'){
-            result += '0';
-        }else if(value === '0'){
-            result += '5';
-        }else{
-            result += '2';
-        }
+  let result = "";
+  for (let value of rsp) {
+    if (value === "2") {
+      result += "0";
+    } else if (value === "0") {
+      result += "5";
+    } else {
+      result += "2";
     }
-    return result;
+  }
+  return result;
 }
