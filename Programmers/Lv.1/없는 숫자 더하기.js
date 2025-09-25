d0933292731bd734ebf804608be5b8e1f62c8d7c
cks-wls <slo0521@naver.com>
2025-09-25 17:13:32 +0900

    [Lv.1] 없는 숫자 더하기

--- Programmers/Lv.1/없는 숫자 더하기.js
+++ Programmers/Lv.1/없는 숫자 더하기.js
@@ -1,9 +1,21 @@
+// sol1
 function solution(numbers) {
-    let result = 0;
-    for (let i = 0; i <= 9; i++){
-        if (!numbers.includes(i)){
-            result += i;
-        }
+  let result = 0;
+  for (let i = 0; i <= 9; i++) {
+    if (!numbers.includes(i)) {
+      result += i;
     }
-    return result;
+  }
+  return result;
+}
+// sol2
+function solution(numbers) {
+  const totalNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let answer = 0;
+  for (let value of totalNumber) {
+    if (!numbers.includes(value)) {
+      answer += value;
+    }
+  }
+  return answer;
 }
