44e4a8547d6a7250094754dcd70f9590a7f5f412
cks-wls <slo0521@naver.com>
2025-09-26 16:35:30 +0900

    [Lv.0] 잘라서 배열로 저장하기

--- Programmers/Lv.0/잘라서 배열로 저장하기.js
+++ Programmers/Lv.0/잘라서 배열로 저장하기.js
@@ -1,19 +1,36 @@
+// solution 1
 function solution(my_str, n) {
-    const result = [];
-    let str = '';
-    for (let i = 0; i < my_str.length; i++){
-        if (i !== my_str.length-1){
-            if (i % n !== n-1){
-                str += my_str[i];
-            }else{
-                str += my_str[i];
-                result.push(str);
-                str = '';
-            }   
-        }else{
-            str += my_str[i];
-            result.push(str);
-        }
+  const result = [];
+  let str = "";
+  for (let i = 0; i < my_str.length; i++) {
+    if (i !== my_str.length - 1) {
+      if (i % n !== n - 1) {
+        str += my_str[i];
+      } else {
+        str += my_str[i];
+        result.push(str);
+        str = "";
+      }
+    } else {
+      str += my_str[i];
+      result.push(str);
     }
-    return result;
+  }
+  return result;
+}
+// solution 2
+function solution(my_str, n) {
+  let ind = n - 1;
+  const result = [];
+  let middle = "";
+  for (let i = 0; i < my_str.length; i++) {
+    if (i % n === ind || i === my_str.length - 1) {
+      middle += my_str[i];
+      result.push(middle);
+      middle = "";
+    } else {
+      middle += my_str[i];
+    }
+  }
+  return result;
 }
