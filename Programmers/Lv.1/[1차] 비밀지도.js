a8bcb4c7a93336bee5fe409db01911e9d0800789
cks-wls <slo0521@naver.com>
2025-10-10 11:00:00 +0900

    [Lv.1] 비밀지도

--- Programmers/Lv.1/[1차] 비밀지도.js
+++ Programmers/Lv.1/[1차] 비밀지도.js
@@ -1,41 +1,42 @@
+// solution1
 function solution(n, arr1, arr2) {
-    const map1 = [];
-    const map2 = [];
-    const result = [];
-    for (let value of arr1){
-        let str = '';
-        for (let i = n-1; i >= 0; i--){
-            if (value >= 2**i){
-                str += 1;
-                value -= 2**i;
-            }else{
-                str += 0;
-            }
-        }
-        map1.push(str);
+  const map1 = [];
+  const map2 = [];
+  const result = [];
+  for (let value of arr1) {
+    let str = "";
+    for (let i = n - 1; i >= 0; i--) {
+      if (value >= 2 ** i) {
+        str += 1;
+        value -= 2 ** i;
+      } else {
+        str += 0;
+      }
     }
-    for (let value of arr2){
-        let str = '';
-        for (let i = n-1; i >= 0; i--){
-            if (value >= 2**i){
-                str += 1;
-                value -= 2**i;
-            }else{
-                str += 0;
-            }
-        }
-        map2.push(str);
+    map1.push(str);
+  }
+  for (let value of arr2) {
+    let str = "";
+    for (let i = n - 1; i >= 0; i--) {
+      if (value >= 2 ** i) {
+        str += 1;
+        value -= 2 ** i;
+      } else {
+        str += 0;
+      }
     }
-    for (let i = 0; i < map1.length; i++){
-        let answer = '';
-        for(let j = 0; j < n; j++){
-            if (map1[i][j] === map2[i][j] && map1[i][j] ==='0'){
-                answer += ' ';
-            }else{
-                answer += '#';
-            }
-        }
-        result.push(answer);
+    map2.push(str);
+  }
+  for (let i = 0; i < map1.length; i++) {
+    let answer = "";
+    for (let j = 0; j < n; j++) {
+      if (map1[i][j] === map2[i][j] && map1[i][j] === "0") {
+        answer += " ";
+      } else {
+        answer += "#";
+      }
     }
-    return result;
+    result.push(answer);
+  }
+  return result;
 }
