4aa830acd46453b4ca66824fcdc92a887e8b3d97
cks-wls <slo0521@naver.com>
2025-10-13 11:00:00 +0900

    [Lv.1] 가운데 글자 가져오기

--- Programmers/Lv.1/가운데 글자 가져오기.js
+++ Programmers/Lv.1/가운데 글자 가져오기.js
@@ -1,10 +1,11 @@
+// solution 1
 function solution(s) {
-    let result = '';
-    if (s.length % 2 === 0){
-        result += s[s.length/2 - 1];
-        result += s[s.length/2];
-    }else{
-        result += s[Math.floor(s.length / 2)];
-    }
-    return result;
+  let result = "";
+  if (s.length % 2 === 0) {
+    result += s[s.length / 2 - 1];
+    result += s[s.length / 2];
+  } else {
+    result += s[Math.floor(s.length / 2)];
+  }
+  return result;
 }
