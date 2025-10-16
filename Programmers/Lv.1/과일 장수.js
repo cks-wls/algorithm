a41dd8f1ac233159afb3ae0e9d44b247eb15425b
cks-wls <slo0521@naver.com>
2025-10-16 11:00:00 +0900

    [Lv.1] 과일 장수

--- Programmers/Lv.1/과일 장수.js
+++ Programmers/Lv.1/과일 장수.js
@@ -1,19 +1,20 @@
+// solution 1
 function solution(k, m, score) {
-    let result = 0;
-    score.sort((a,b) =>b-a);
-    const box = [];
-    for (let i = 0; i < score.length; i += m){
-        let middle = [];
-        for(let j = 0; j < m; j++){
-            middle.push(score[i + j]);
-        }
-         box.push(middle); 
+  let result = 0;
+  score.sort((a, b) => b - a);
+  const box = [];
+  for (let i = 0; i < score.length; i += m) {
+    let middle = [];
+    for (let j = 0; j < m; j++) {
+      middle.push(score[i + j]);
     }
-    for (let value of box){
-        if (!value.includes(undefined)){
-            let min = Math.min(...value);
-            result += min * m;
-        }
+    box.push(middle);
+  }
+  for (let value of box) {
+    if (!value.includes(undefined)) {
+      let min = Math.min(...value);
+      result += min * m;
     }
-    return result;
+  }
+  return result;
 }
