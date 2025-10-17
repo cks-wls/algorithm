4c8719acd2ab99d840006b4e8cf73c7c77ad9f09
cks-wls <slo0521@naver.com>
2025-10-17 11:00:00 +0900

    [Lv.1] 기사단원의 무기

--- Programmers/Lv.1/기사단원의 무기.js
+++ Programmers/Lv.1/기사단원의 무기.js
@@ -1,30 +1,31 @@
+// solution 1
 function solution(number, limit, power) {
-    const arr = [];
-    let answer = 0;
-    for (let i = 1; i <= number; i++){
-        arr.push(measure(i));
+  const arr = [];
+  let answer = 0;
+  for (let i = 1; i <= number; i++) {
+    arr.push(measure(i));
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > limit) {
+      arr[i] = power;
     }
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] > limit){
-            arr[i] = power;
-        }
-    }
-    for (let value of arr){
-        answer += value;
-    }
-    return answer;
+  }
+  for (let value of arr) {
+    answer += value;
+  }
+  return answer;
 }
-function measure(val){
-    let count = 0;
-    let sqrt = Math.sqrt(val)
-     for (let i = 1; i <= sqrt; i++) {
-        if (val % i === 0) {
-            if (i === sqrt) {
-                count += 1;
-            } else {
-                count += 2;
-            }
-        }
+function measure(val) {
+  let count = 0;
+  let sqrt = Math.sqrt(val);
+  for (let i = 1; i <= sqrt; i++) {
+    if (val % i === 0) {
+      if (i === sqrt) {
+        count += 1;
+      } else {
+        count += 2;
+      }
     }
-    return count ;
+  }
+  return count;
 }
