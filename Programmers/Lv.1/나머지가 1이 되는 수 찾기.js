6e0fbc3a5cde050cbcd53a5e4f22ed6d15129955
cks-wls <slo0521@naver.com>
2025-10-19 11:00:00 +0900

    [Lv.1] 나머지가 1이 되는 수 찾기

--- Programmers/Lv.1/나머지가 1이 되는 수 찾기.js
+++ Programmers/Lv.1/나머지가 1이 되는 수 찾기.js
@@ -1,7 +1,8 @@
+// solution 1
 function solution(n) {
-    let x = 2;
-    while (n % x !==1){
-        x ++;
-    }
-    return x;
+  let x = 2;
+  while (n % x !== 1) {
+    x++;
+  }
+  return x;
 }
