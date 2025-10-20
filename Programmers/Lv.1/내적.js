78779868ea1049a3520054ccd9bf8bf2e602a3b6
cks-wls <slo0521@naver.com>
2025-10-20 11:00:00 +0900

    [Lv.1] 내적

--- Programmers/Lv.1/내적.js
+++ Programmers/Lv.1/내적.js
@@ -1,7 +1,8 @@
+// solution 1
 function solution(a, b) {
-    let result = 0;
-    for (let i = 0; i < a.length; i++){
-        result += a[i]*b[i];
-    }
-    return result;
+  let result = 0;
+  for (let i = 0; i < a.length; i++) {
+    result += a[i] * b[i];
+  }
+  return result;
 }
