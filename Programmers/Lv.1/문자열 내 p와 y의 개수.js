51202fc38b894371af7d475c738c2487f34854eb
cks-wls <slo0521@naver.com>
2025-10-24 11:00:00 +0900

    [Lv.1] 문자열 내 p와 y의 개수

--- Programmers/Lv.1/문자열 내 p와 y의 개수.js
+++ Programmers/Lv.1/문자열 내 p와 y의 개수.js
@@ -1,20 +1,21 @@
-function solution(s){
-    let pCount = 0;
-    let yCount = 0;
-    for (let value of s){
-        if (value === 'p' || value === 'P'){
-            pCount += 1;
-        }else if (value === 'y' || value === 'Y'){
-            yCount += 1;
-        }
+// solution 1
+function solution(s) {
+  let pCount = 0;
+  let yCount = 0;
+  for (let value of s) {
+    if (value === "p" || value === "P") {
+      pCount += 1;
+    } else if (value === "y" || value === "Y") {
+      yCount += 1;
     }
-    if (pCount === 0 && yCount === 0){
-        return  true;
-    }else{
-     if (pCount === yCount){
-        return  true;
-    }else{
-        return  false;
-    }   
+  }
+  if (pCount === 0 && yCount === 0) {
+    return true;
+  } else {
+    if (pCount === yCount) {
+      return true;
+    } else {
+      return false;
     }
+  }
 }
