711eb6cdfba47e2db73292b38cd024f17f5329d0
cks-wls <slo0521@naver.com>
2025-11-04 11:00:00 +0900

    [Lv.1] 부족한 금액 계산하기

--- Programmers/Lv.1/부족한 금액 계산하기.js
+++ Programmers/Lv.1/부족한 금액 계산하기.js
@@ -1,11 +1,12 @@
+// solution 1
 function solution(price, money, count) {
-    let totalPrice = 0;
-    for (let i = 1; i <= count; i++){
-        totalPrice += i*price;
-    }
-    if (money >= totalPrice){
-        return 0;
-    }else{
-        return totalPrice - money;
-    }
+  let totalPrice = 0;
+  for (let i = 1; i <= count; i++) {
+    totalPrice += i * price;
+  }
+  if (money >= totalPrice) {
+    return 0;
+  } else {
+    return totalPrice - money;
+  }
 }
