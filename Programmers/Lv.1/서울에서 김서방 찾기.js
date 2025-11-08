99a5766542bd8d97f0445c5c45976751d72a302b
cks-wls <slo0521@naver.com>
2025-11-08 11:00:00 +0900

    [Lv.1] 서울에서 김서방 찾기

--- Programmers/Lv.1/서울에서 김서방 찾기.js
+++ Programmers/Lv.1/서울에서 김서방 찾기.js
@@ -1,5 +1,6 @@
+// solution 1
 function solution(seoul) {
-    let index = 0;
-    index = seoul.indexOf('Kim');
-    return (`김서방은 ${index}에 있다`)
+  let index = 0;
+  index = seoul.indexOf("Kim");
+  return `김서방은 ${index}에 있다`;
 }
