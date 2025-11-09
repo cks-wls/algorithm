348cf1c32708357fc3c66d89ad1d057c40d0aa52
cks-wls <slo0521@naver.com>
2025-11-09 11:00:00 +0900

    [Lv.1] 숫자 문자열과 영단어

--- Programmers/Lv.1/숫자 문자열과 영단어.js
+++ Programmers/Lv.1/숫자 문자열과 영단어.js
@@ -1,29 +1,30 @@
+// solution 1
 function solution(s) {
-    const number = {
-        'zero' : 0,
-        'one' : 1,
-        'two' : 2,
-        'three' : 3,
-        'four' : 4,
-        'five' : 5,
-        'six' : 6,
-        'seven' : 7,
-        'eight' : 8,
-        'nine' : 9
+  const number = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  const numStr = Object.keys(number);
+  let result = "";
+  let midResult = "";
+  for (let value of s) {
+    if (!isNaN(Number(value))) {
+      result += value;
+    } else {
+      midResult += value;
+      if (numStr.includes(midResult)) {
+        result += number[midResult];
+        midResult = "";
+      }
     }
-    const numStr = Object.keys(number);
-    let result = '';
-    let midResult = '';
-    for (let value of s){
-        if (!isNaN(Number(value))){
-            result += value;
-        }else{
-            midResult += value;
-            if (numStr.includes(midResult)){
-                result += number[midResult];
-                midResult = '';
-            }
-     }
-    }
-    return Number(result);
+  }
+  return Number(result);
 }
